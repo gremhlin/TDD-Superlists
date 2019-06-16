609b10105ffbdd53bfcacaaf018291b453b4878c
George Krogh <bork.jorgen@gmail.com>
2019-06-16 15:34:06 +0200

    First unit test and url mapping, dummy view!

--- superlists/urls.py
+++ superlists/urls.py
@@ -9,13 +9,13 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
+Including another URLconf+
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from lists import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
 ]
